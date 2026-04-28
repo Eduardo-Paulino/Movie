90e7ec1283804b64f3a5a240621be7843c1aca89
Luis Eduardo Del Razo Paulino <22470313@campeche.tecnm.mx>
2026-04-27 23:12:10 -0600

    Agregar CRUD para las peliculas

--- Movie/Program.cs
+++ Movie/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Movie.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieContext") ?? throw new InvalidOperationException("Connection string 'MovieContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
